b7e6a2f2ee9aea48739f8fee34a92c98b15700ee
Madiyor <abdukhashimov@yandex.ru>
2020-02-10 18:13:54 +0500

    created post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        if len(str(self.title)) > 20:
+            return self.title[:20] + '...'
+        return self.title
